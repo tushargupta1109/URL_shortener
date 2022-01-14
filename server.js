e72bdb5043bf3d9dc821217dbdd4a96e8b8e0ac8
unknown <guptat320@gmail.com>
2022-01-14 16:34:55 +0530

    frontend work

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const app = express();
 mongoose.connect("mongodb://localhost/urlShortener", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).then(res=>{
+  console.log("DB Connected!")
+}).catch(err => {
+console.log(Error, err.message);
+})
 
 app.set("view engine", "ejs");
 // app.use(express.urlendcoded({ extended: false }));
@@ -20,6 +24,17 @@ app.post("/shortUrls", async (req, res) => {
   await ShortUrl.create({ full: req.body.fullUrl });
   res.redirect("/");
 });
+
+app.get('/:shortUrl',async (req,res)=>{
+  const shortUrl=await ShortUrl.findOne({short:req.params.shortUrl})
+  if(shortUrl== null){
+    return res.sendStatus(404);
+  }
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+})
 
 app.listen(8000, () => {
   console.log(`Server is running on port 8000`);
